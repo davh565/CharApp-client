cb8518ec4ab50151fcc0c8a6a2d45439c0377726
David Humphries <d.a.humphries@gmail.comname>
2019-01-05 19:34:09 +1300

    added socket io communication

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,55 @@
   <div id="app">
     <router-link to="/"><h1>CharApp</h1></router-link>
     <router-view></router-view>
+    <p v-if="isConnected">We're connected to the server!</p>
+    <input v-model="socketMessage" @blur="sendMessage">
+    <p>Messages from server:</p>
+    <ul>
+      <li v-for="(message, key) in messages" :key="key">
+        <p>{{message.message}}</p>
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
-import MenuMain from './components/MenuMain.vue'
+import io from 'socket.io-client'
 
 export default {
   name: 'app',
   components: {
-    MenuMain
-  }
+  },
+
+  data() {
+    return {
+      isConnected: false,
+      messages: [],
+      socketMessage: '',
+      socket: io('localhost:3000')
+    }
+  },
+    sockets: {
+    connect: function () {
+        console.log('socket connected')
+    },
+    customEmit: function (data) {
+        console.log('this method was fired by the socket server. eg: io.emit("customEmit", data)')
+    }
+},
+methods: {
+        sendMessage(e) {
+            e.preventDefault();
+            
+            this.socket.emit('chat message', this.socketMessage);
+            this.socketMessage = ''
+        }
+    },
+    mounted() {
+        this.socket.on('chat message', (data) => {
+            this.messages = [...this.messages, data];
+            // you can also do this.messages.push(data)
+        });
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Routes from './routes.js'
-import routes from './routes.js';
+// import VueSocketIO from 'vue-socket.io';
+
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+// Vue.use(new VueSocketIO({
+//   debug: true,
+//   connection: 'http://localhost:3000'
+// }))
 
 const router = new VueRouter({
   routes: Routes,
