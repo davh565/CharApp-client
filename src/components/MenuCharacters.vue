c22d02c8ff8ee811a2c751e04d481130a87a5a4d
Dave Humphries <d.a.humphries@gmail.com>
2019-01-13 22:57:57 +1300

    cruD

--- src/components/MenuCharacters.vue
+++ src/components/MenuCharacters.vue
@@ -1,14 +1,72 @@
 <template>
-  <div class="hello">
-    <h1>Characters</h1>
+  <v-container
+  fluid
+  grid-list-md
+  text-xs-center>
+    <v-layout
+    row
+    wrap
+    justify-center
+    >
+      <v-flex
+      xs12>
+        <h1>Characters</h1>
+        <v-btn
+        large 
+        color="blue"
+        to="/characters/add">New Character</v-btn>
+        <br><br>
+        </v-flex>
     <br>
-    <v-btn large to="/characters/add">New Character</v-btn><br><br>
-    <li v-for="character in characters" :key=character._id>
-      <v-btn :to="'/characters/'+character._id">
-        {{character.characterName}}
+    <!--<li v-for="character in characters" :key=character._id>
+      
+    </li><br> -->
+    <v-flex
+      align-self-center
+        xs12
+        sm6
+        md4
+        v-for="character in characters" 
+        :key=character._id>
+    <v-card
+    
+     >
+        <!-- <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img> -->
+
+        <v-card-title 
+        class="justify-center" primary-title>
+          <div>
+            <h3 class="headline mb-0">
+              {{character.characterName}}
+            </h3>
+            <div>{{character.ruleset}}<br>
+            {{character.campaign}}</div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn
+          flat
+          color="blue"
+          :to="'/characters/'+character._id">
+        Select
       </v-btn>
-    </li><br>
-  </div>
+          <v-btn
+            flat
+            color="red"
+            @click="deleteChar(character._id)"
+            
+          >
+            Delete
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -24,6 +82,12 @@ export default {
     characters: function(){
       return this.$store.state.characters
     },
+  },
+  methods: {
+    deleteChar: function(id){
+      console.log(id, typeof(id))
+      this.$socket.emit('delCharacter',id)
+    }
   }
 }
 </script>
